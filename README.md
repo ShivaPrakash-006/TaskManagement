5abe258a8fd21ce0e6155edacbf853b4a28bdb4f
ShivaPrakash-006 <shivapraksh2006@gmail.com>
2025-04-22 19:35:59 +0530

    Added Groups

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 - [TASK MANAGEMENT](#task-management)
   - [FEATURES](#features)
   - [IMPLEMENTATION/ALGORITHM/GENERAL IDEA](#implementationalgorithmgeneral-idea)
+  - [WORKING](#working)
+  - [Bugs](#bugs)
   <!--toc:end-->
 
 Create A Task Manager for Your Daily needs.
@@ -12,17 +14,17 @@ Create A Task Manager for Your Daily needs.
 ## FEATURES
 
 - [ ] Beautiful TUI
-- [ ] Add any task to the list.
-- [ ] Group tasks (Linked List?).
+- [x] Add any task to the list.
+- [-] Group tasks (Linked List?).
+  - [x] Create and Assign Groups
+  - [-] Filter by Group
 - [ ] Link tasks (Graphs).
 - [ ] Set deadlines for tasks.
 - [ ] Set priority for tasks.
 - [ ] Different Views:
-
   - [ ] All
   - [ ] Group
   - [ ] Filter
-
 - [ ] Sort By Deadlines/Priority.
 - [ ] Sub Tasks(?).
 - [ ] Set Tasks Active/Inactive.
@@ -32,3 +34,14 @@ Create A Task Manager for Your Daily needs.
 
 The Popular `ncurses` library is used for the TUI.
 Use Linked Lists to Store Tasks. External Storage of Tasks will use (JSON/SQLite).
+
+## WORKING
+
+There are two windows on startup: The Task Window which shows all the Tasks and the Description Window which shows the all the details of the window. Pressing _**F1**_ adds a new Task to the list and _**F2**_ deletes the currently selected task from the list.
+Press F10 to exit
+
+## Bugs
+
+- Pressing Arrow Keys while task list empty crashes
+- Does not automatically clear description screen
+-
--- main.c
+++ main.c
@@ -5,27 +5,269 @@
 #include <ncurses.h>
 #include <panel.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 typedef struct Task {
   char title[32];
-  char desc[128];
+  char desc[512];
   char group[32];
   struct Task *linkedTasks;
   struct Task *nextTask;
 } Task;
 
-Task *createTask(WINDOW *window) {
-  Task *newTask = (Task *)malloc(sizeof(Task));
+typedef struct Group {
+  char name[32];
+  struct Group *nextGroup;
+} Group;
+
+void clearWindow(WINDOW *window) {
+  wclear(window);
+  box(window, 0, 0);
+}
+
+void printTitle(WINDOW *window, char *text) {
+  int width = getmaxx(window);
+  mvwprintw(window, 0, (width - strlen(text)) / 2 - 1, " %s ", text);
+}
+
+void printWrapped(char *text, WINDOW *window, int startx, int starty) {
+  char *letter = text;
+  int x = startx, y = starty, width = getmaxx(window);
+  wmove(window, startx, starty);
+
+  while (*letter) {
+    if (x >= width - 1) {
+      y++;
+      x = startx;
+    }
+
+    mvwaddch(window, y, x++, *letter++);
+    wrefresh(window);
+  }
+}
+
+void printTasks(Task *head, WINDOW *window, int currentTaskNo) {
+  Task *temp = head;
+  int taskNo = 0;
+  wclear(window);
+  box(window, 0, 0);
+  printTitle(window, "Tasks");
+  wmove(window, 1, 1);
+  while (temp != NULL) {
+    if (currentTaskNo == taskNo) {
+      wattron(window, A_STANDOUT);
+      wprintw(window, "%s", temp->title);
+      wattroff(window, A_STANDOUT);
+    } else
+      wprintw(window, "%s", temp->title);
+    temp = temp->nextTask;
+    wmove(window, ++taskNo + 1, 1);
+  }
+  wrefresh(window);
+}
+
+void printInsertMenu(WINDOW *window, int currentItemNo) {
+  char *items[] = {"Title", "Description", "Group", "Add", "Cancel"};
+  int noOfItems = 5;
+  int itemNo = 0;
+  wclear(window);
+  box(window, 0, 0);
+  printTitle(window, "Add Task");
+  wmove(window, 1, 1);
+  while (itemNo < noOfItems) {
+    if (currentItemNo == itemNo) {
+      wattron(window, A_STANDOUT);
+      wprintw(window, "%s", items[itemNo]);
+      wattroff(window, A_STANDOUT);
+    } else
+      wprintw(window, "%s", items[itemNo]);
+    wmove(window, ++itemNo + 1, 1);
+  }
+  wrefresh(window);
+}
+
+void printDesc(Task *task, WINDOW *window) {
+  wclear(window);
+  box(window, 0, 0);
+  printTitle(window, "Description");
+  wmove(window, 1, 1);
+  printWrapped(task->desc, window, 1, 1);
+  wrefresh(window);
+}
+
+void printField(WINDOW *window, Task *task, int fieldNo) {
   wclear(window);
   box(window, 0, 0);
   wmove(window, 1, 1);
-  wprintw(window, "Title: ");
-  wgetnstr(window, newTask->title, 32);
-  wmove(window, 2, 1);
-  wprintw(window, "Description: ");
-  wgetnstr(window, newTask->desc, 128);
+  switch (fieldNo) {
+  case 0:
+    wprintw(window, "%s", task->title);
+    break;
+
+  case 1:
+    printWrapped(task->desc, window, 1, 1);
+    break;
+
+  case 2:
+    wprintw(window, "%s", task->group);
+    break;
+
+  case 3:
+    wprintw(window, "Add Task");
+  }
+  wrefresh(window);
+}
+
+void printGroups(WINDOW *window, Group *groups, int currentGroupNo) {
+  Group *group = groups;
+  char *items[] = {"Create", "Cancel"};
+  int groupNo = 0;
+  wclear(window);
+  box(window, 0, 0);
+  printTitle(window, "Groups");
+  wmove(window, 1, 1);
+  while (group != NULL || groupNo < 2) {
+    if (groupNo < 2) {
+      if (currentGroupNo == groupNo) {
+        wattron(window, A_STANDOUT);
+        wprintw(window, "%s", items[groupNo]);
+        wattroff(window, A_STANDOUT);
+      } else {
+        wprintw(window, "%s", items[groupNo]);
+      }
+    } else {
+      if (currentGroupNo == groupNo) {
+        wattron(window, A_STANDOUT);
+        wprintw(window, "%s", group->name);
+        wattroff(window, A_STANDOUT);
+      } else
+        wprintw(window, "%s", group->name);
+      group = group->nextGroup;
+    }
+    wmove(window, ++groupNo + 1, 1);
+  }
+  wrefresh(window);
+}
+
+void createGroup(Group **head, WINDOW *window) {
+  clearWindow(window);
+  printTitle(window, "Create Group");
+  Group *newGroup = (Group *)malloc(sizeof(Group));
+  mvwprintw(window, 1, 1, "Enter Name of Group: ");
+  wgetnstr(window, newGroup->name, sizeof(newGroup->name));
+  // Inserting
+  newGroup->nextGroup = *head;
+  *head = newGroup;
+}
+
+Group *getGroup(Group *groups, int groupNo) {
+  int i = 0;
+  Group *temp = groups;
+  while (i < groupNo) {
+    temp = temp->nextGroup;
+    i++;
+  }
+  return temp;
+}
+
+Group *selectGroup(Group **groups, WINDOW *window, unsigned int *size) {
+  bool selecting = true;
+  int currentGroupNo = 0, keyPress;
+  int selectedGroupNo = -1;
+  while (selecting) {
+    printGroups(window, *groups, currentGroupNo);
+    keyPress = getch();
+    switch (keyPress) {
+    case KEY_UP:
+      if (currentGroupNo != 0)
+        currentGroupNo--;
+      else
+        currentGroupNo = (*size) + 1;
+      break;
+
+    case KEY_DOWN:
+      if (currentGroupNo != (*size) + 1)
+        currentGroupNo++;
+      else
+        currentGroupNo = 0;
+      break;
+    case 10:
+      selectedGroupNo = currentGroupNo;
+      break;
+    }
+
+    if (selectedGroupNo == 0) {
+      createGroup(groups, window);
+      (*size)++;
+    } else if (selectedGroupNo == 1) {
+      return NULL;
+    } else if (selectedGroupNo > 1) {
+      selecting = false;
+    }
+    selectedGroupNo = -1;
+  }
+  return getGroup(*groups, selectedGroupNo);
+}
+
+Task *createTask(WINDOW *menuWindow, WINDOW *detailWindow, Group **groups,
+                 unsigned int *groupListSize) {
+  Task *newTask = (Task *)malloc(sizeof(Task));
+  wclear(menuWindow);
+  wclear(detailWindow);
+  box(menuWindow, 0, 0);
+  box(detailWindow, 0, 0);
+  printTitle(menuWindow, "Add Task");
+
+  bool creating = true;
+  int currentItemNo = 0, keyPress;
+  int selectedItemNo = -1;
+  while (creating) {
+    printInsertMenu(menuWindow, currentItemNo);
+    keyPress = getch();
+    switch (keyPress) {
+    case KEY_UP:
+      if (currentItemNo != 0)
+        currentItemNo--;
+      printField(detailWindow, newTask, currentItemNo);
+      break;
+    case KEY_DOWN:
+      if (currentItemNo != 4)
+        currentItemNo++;
+      printField(detailWindow, newTask, currentItemNo);
+      break;
+    case 10:
+      selectedItemNo = currentItemNo;
+      break;
+    }
+    if (selectedItemNo == 3)
+      creating = false;
+    else if (selectedItemNo == 0) {
+      curs_set(1);
+      wmove(detailWindow, 1, 1);
+      wgetnstr(detailWindow, newTask->title, sizeof(newTask->title));
+      curs_set(0);
+    } else if (selectedItemNo == 1) {
+      curs_set(1);
+      wmove(detailWindow, 1, 1);
+      wgetnstr(detailWindow, newTask->desc, sizeof(newTask->desc));
+      curs_set(0);
+    } else if (selectedItemNo == 2) {
+      strcpy(newTask->group,
+             selectGroup(groups, detailWindow, groupListSize)->name);
+    } else if (selectedItemNo == 4) {
+      wclear(detailWindow);
+      box(detailWindow, 0, 0);
+      printTitle(detailWindow, "Delete");
+      mvwprintw(detailWindow, 1, 1, "Are you sure? (y/n)");
+      char choice = wgetch(detailWindow);
+
+      if (choice == 'y')
+        return NULL;
+    }
+    selectedItemNo = -1;
+  }
   newTask->nextTask = NULL;
   return newTask;
 }
@@ -44,6 +286,7 @@ void insert(Task **head, Task *newTask) {
 void delete(Task **head, int pos, WINDOW *window) {
   wclear(window);
   box(window, 0, 0);
+  printTitle(window, "Delete");
   mvwprintw(window, 1, 1, "Are you sure? (y/n)");
   char choice = wgetch(window);
 
@@ -92,40 +335,14 @@ Task *getTask(Task *head, int taskNo) {
   return temp;
 }
 
-void printMenu(Task *head, WINDOW *window, int currentTaskNo) {
-  Task *temp = head;
-  int taskNo = 0;
-  wclear(window);
-  box(window, 0, 0);
-  wmove(window, 1, 1);
-  while (temp != NULL) {
-    if (currentTaskNo == taskNo) {
-      wattron(window, A_STANDOUT);
-      wprintw(window, "%s", temp->title);
-      wattroff(window, A_STANDOUT);
-    } else
-      wprintw(window, "%s", temp->title);
-    temp = temp->nextTask;
-    wmove(window, ++taskNo + 1, 1);
-  }
-  wrefresh(window);
-}
-
-void printDesc(Task *task, WINDOW *window) {
-  wclear(window);
-  box(window, 0, 0);
-  wmove(window, 1, 1);
-  wprintw(window, "%s", task->desc);
-  wrefresh(window);
-}
-
 int main() {
-  Task *head = NULL, *newTask;
+  Task *taskHead = NULL, *newTask;
+  Group *groupHead = NULL, *newGroup;
 
   int choice;
   bool deleteMode = false, run = true;
   char title[32], desc[128];
-  unsigned int size = 0, currentTaskNo = 0, task = 0;
+  unsigned int size = 0, currentTaskNo = 0, task = 0, groupListSize = 0;
 
   initscr();
   keypad(stdscr, true);
@@ -142,7 +359,7 @@ int main() {
   wrefresh(descWindow);
 
   while (run) {
-    printMenu(head, taskWindow, currentTaskNo);
+    printTasks(taskHead, taskWindow, currentTaskNo);
     choice = getch();
     switch (choice) {
     case KEY_UP:
@@ -150,18 +367,19 @@ int main() {
         currentTaskNo = size - 1;
       else
         currentTaskNo--;
-      printDesc(getTask(head, currentTaskNo), descWindow);
+      printDesc(getTask(taskHead, currentTaskNo), descWindow);
       break;
     case KEY_DOWN:
       if (currentTaskNo == size - 1)
         currentTaskNo = 0;
       else
         currentTaskNo++;
-      printDesc(getTask(head, currentTaskNo), descWindow);
+      printDesc(getTask(taskHead, currentTaskNo), descWindow);
       break;
     case KEY_F(1):
-      newTask = createTask(taskWindow);
-      insert(&head, newTask);
+      newTask = createTask(taskWindow, descWindow, &groupHead, &groupListSize);
+      if (newTask)
+        insert(&taskHead, newTask);
       size++;
       break;
     case KEY_F(2):
@@ -177,7 +395,7 @@ int main() {
     }
 
     if (deleteMode) {
-      delete (&head, currentTaskNo, taskWindow);
+      delete (&taskHead, currentTaskNo, taskWindow);
       if (currentTaskNo == size - 1)
         currentTaskNo--;
       size--;
